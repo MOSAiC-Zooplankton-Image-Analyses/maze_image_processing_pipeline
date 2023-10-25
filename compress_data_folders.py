a71c18a0cbc3aa07fbf6601c4d388f7a0b71ba92
Simon-Martin Schroeder <sms@informatik.uni-kiel.de>
2023-10-25 20:54:21 +0200

    compress data folders: Ignore pattern and detect duplicate target fn

--- compress_data_folders.py
+++ compress_data_folders.py
@@ -11,8 +11,9 @@ from tqdm.auto import tqdm
 @click.argument('root_dir')
 @click.option('-j', "--n_workers", type=int, default=1)
 @click.option("--skip-existing", is_flag=True)
+@click.option("--ignore", multiple=True)
 #TODO: Skip existing
-def main(root_dir, n_workers, skip_existing):
+def main(root_dir, n_workers, skip_existing, ignore):
     """
     Find and compress LOKI data folders.
     
@@ -24,12 +25,18 @@ def main(root_dir, n_workers, skip_existing):
 
     print("Discovering project directories...")
     futures: List[concurrent.futures.Future] = []
-    for data_root in find_data_roots(root_dir, progress=False):
+    existing_archive_fns = set()
+    for data_root in find_data_roots(root_dir, progress=False, ignore_patterns=ignore):
         (log_fn,) = glob.glob(os.path.join(data_root, "Log", "LOKI*.log"))
         log = read_log(log_fn)
         sample_id = "{STATION}_{HAUL}".format_map(log)
 
         archive_fn = os.path.join(root_dir, sample_id) + ".zip"
+
+        if archive_fn in existing_archive_fns:
+            raise click.Abort(f"Duplicate target archive filename {archive_fn}")
+
+        existing_archive_fns.add(archive_fn)
 
         if skip_existing and os.path.isfile(archive_fn):
             print(archive_fn, "already exists.")
@@ -40,7 +47,7 @@ def main(root_dir, n_workers, skip_existing):
         future.add_done_callback(lambda _, sample_id=sample_id: print(sample_id, "finished.\n"))
         futures.append(future)
 
-    print("Waiting for compression to finish...")
+    print("Compressing...")
     for _ in tqdm(concurrent.futures.as_completed(futures), total=len(futures)):
         pass
 
